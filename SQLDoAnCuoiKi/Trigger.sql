9d7ec74e1ae86588b13009e36d90ed47d86f169b
BaoNun1205 <94816323+BaoNun1205@users.noreply.github.com>
2023-11-07 10:49:50 +0700

    thêm check product

--- SQLDoAnCuoiKi/Trigger.sql
+++ SQLDoAnCuoiKi/Trigger.sql
@@ -1,4 +1,53 @@
 --Trigger
+--Product
+--Kiểm tra trước khi thêm hoặc sửa thông tin mặt hàng
+CREATE TRIGGER trg_CheckProduct
+ON PRODUCT
+AFTER INSERT, UPDATE
+AS
+BEGIN
+	
+	IF EXISTS (SELECT * FROM inserted WHERE TRIM(p_id) = '')
+    BEGIN
+        RAISERROR('Loại mặt hàng không được để trống', 16, 1)
+        ROLLBACK TRANSACTION
+        RETURN
+    END
+
+    -- Kiểm tra p_name
+    IF EXISTS (SELECT * FROM inserted WHERE TRIM(p_name) = '')
+    BEGIN
+        RAISERROR('Tên mặt hàng không được để trống', 16, 1)
+        ROLLBACK TRANSACTION
+        RETURN
+    END
+
+    -- Kiểm tra p_price
+    IF EXISTS (SELECT * FROM inserted WHERE p_price <= 0)
+    BEGIN
+        RAISERROR('Giá mặt hàng phải lớn hơn 0', 16, 1)
+        ROLLBACK TRANSACTION
+        RETURN
+    END
+
+    -- Kiểm tra p_size
+    IF EXISTS (SELECT * FROM inserted WHERE TRIM(p_size) = '')
+    BEGIN
+        RAISERROR('Kích thước mặt hàng không được để trống', 16, 1)
+        ROLLBACK TRANSACTION
+        RETURN
+    END
+
+    -- Kiểm tra p_quantity
+    IF EXISTS (SELECT * FROM inserted WHERE p_quantity < 0)
+    BEGIN
+        RAISERROR('Số lượng mặt hàng không được âm', 16, 1)
+        ROLLBACK TRANSACTION
+        RETURN
+    END
+END;
+
+--Cập nhật số lượng mặt hàng sau khi thêm vào kho thành công
 CREATE TRIGGER trig_UpdateWarehouse_AfterShip
 ON Detail_Shipment
 AFTER INSERT
